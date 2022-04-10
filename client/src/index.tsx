45d992eff87e42dd56a5ca82d4ccee00833e2a89
reds0l <10118101+reds0l@users.noreply.github.com>
2022-04-10 12:21:21 -0700

    working websocket connection

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import io from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-const socket = io("ws://localhost:3001")
+////////////////////////
+//     Interfaces     //
+////////////////////////
+
+interface ServerToClientEvents {
+}
+
+interface ClientToServerEvents {
+  hello: (msg: string) => void;
+  incrementCount: (callback: (count: number) => void) => void;
+}
+
+///////////////////////
+//     Functions     //
+///////////////////////
+
+function sayHello(){
+  console.log('sayHello called')
+  socket.emit('hello', 'hello from client')
+}
+
+function incremenetCount() {
+  socket.emit('incrementCount', (response) => {
+    console.log("Count: "+ response)
+  })
+}
+
+///////////////////
+//     Setup     //
+///////////////////
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io("http://localhost:3001");
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <h1> Hello World 1 </h1>
-    <button> Get Count </button>
-    <br></br>
-    <button> Increment Count </button>
+    <button onClick={sayHello}> Send Hello </button>
+    <button onClick={incremenetCount}> Increment Count </button>
   </React.StrictMode>,
 );
--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,75 @@
+'use strict'
+import { Server } from "socket.io";
+
+/////////////////////////
+//     Interfaces     //
+/////////////////////////
+
+interface ServerToClientEvents {
+}
+
+interface ClientToServerEvents {
+  hello: (a: string) => void;
+  incremenetCount: (callback: (e: number) => void) => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+///////////////////
+//     Setup     //
+///////////////////
+
 console.log('Starting server...');
 
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server, 
+  {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
   }
-});
+);
 
 //const PORT = process.env.PORT || "3001";
 const PORT = "3001";
 
-app.get('/', (req: any, res: any) => {
-  res.send("<h1>Hello World 2</h1>");
-});
+let count = 0
 
-io.on('connection', (socket: any) => {
-  console.log('a user connected');
+app.get('/', (req: any, res: any) => {
+  res.send("<h1>Server is running :)</h1>");
 });
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+//////////////////////////////
+//     Socket.io Events     //
+//////////////////////////////
+
+// Never trust user input!
+
+io.on('connection', (socket: any) => {
+  console.log('a user connected');
+
+  socket.on('hello', function(data: any) {
+    console.log(data)
+  })
+
+  socket.on('incrementCount', (callback: Function) => {
+    count = count + 1
+    console.log('incrementing count: '+ count)
+    callback(count)
+  })
+});
+
